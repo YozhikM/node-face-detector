a4ec5f89e57fddb773417843a7ae8e9513cbbad2
YozhikM <botev.st@gmail.com>
2018-02-08 20:35:18 +0600

    wip

--- src/getBuffer.js
+++ src/getBuffer.js
@@ -1,5 +1,3 @@
-/* @flow */
-
 const fs = require('fs');
 const tmp = require('tmp');
 const fr = require('face-recognition');
@@ -14,38 +12,48 @@ function createRectangles(imgPath) {
   return faceRectangles;
 }
 
-// $FlowFixMe
+async function createCroppedImages(faceRectangles, buffer, scale) {
+  const arr = [];
+  faceRectangles.forEach(async (fc, i) => {
+    const { bottom, top, right, left } = fc.rect || {};
+
+    const width = (right - left) * scale;
+    const height = (bottom - top) * scale;
+    const x = left;
+    const y = top;
+
+    await gm(buffer)
+      .crop(width, height, x, y)
+      .toBuffer('JPG', async (error, croppedBuffer) => {
+        if (error) console.log(error);
+        // этого не будет, сделано для проверки
+        // gm(croppedBuffer).write(`./images/face${i + 1}.jpg`, () => {
+        //   console.log(`image #${i + 1} recorded`);
+        // });
+        await gm(croppedBuffer).toBuffer('JPG', async (err, buff) => {
+          await arr.push(buff);
+          console.log(`buff${i}`, buff);
+        });
+      });
+  });
+  console.log('arr: ', arr);
+}
+
 function getBuffer(buffer, scale = 1) {
   const tmpobj = tmp.fileSync({ postfix: '.jpg' });
   const tmpName = tmpobj.name;
+  let result;
 
   fs.writeFile(tmpName, buffer, 'binary', err => {
     if (err) console.log(err);
-    console.log('file created');
 
     const faceRectangles = createRectangles(tmpName);
 
-    faceRectangles.forEach((fc, i) => {
-      const { bottom, top, right, left } = fc.rect || {};
-
-      const width = (right - left) * scale;
-      const height = (bottom - top) * scale;
-      const x = left;
-      const y = top;
-
-      gm(buffer)
-        .crop(width, height, x, y)
-        .toBuffer('JPG', (error, croppedBuffer) => {
-          if (error) console.log(error);
-          // этого не будет, сделано для проверки
-          gm(croppedBuffer).write(`./images/face${i + 1}.jpg`, () => {
-            console.log(`image #${i + 1} created`);
-          });
-        });
-    });
+    createCroppedImages(faceRectangles, buffer, scale);
 
     fs.unlink(tmpName, () => console.log('file deleted'));
   });
+  if (result) console.log(result);
 }
 
 module.exports = getBuffer;
